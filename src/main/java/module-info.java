e10c2ae72c3f35a9e06b8c89792a02d808789fb9
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2023-03-09 13:10:34 +0100

    setting up

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.sofoste.apscalculator {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.sofoste.apscalculator to javafx.fxml;
+    exports com.sofoste.apscalculator;
+}
